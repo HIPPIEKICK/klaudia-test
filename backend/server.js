ee061739e42f491ba083491bb0a786c7ddd7569e
Matilda Brunemalm <51529201+HIPPIEKICK@users.noreply.github.com>
2023-12-07 16:39:53 +0100

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-auth';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+
+import routeTest from './routes/route';
+import userRoute from './routes/users';
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: 'Database is not responding' });
+  }
+});
+
+app.use('/', routeTest);
+app.use('/', userRoute);
+
+// Start defining your routes here
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
